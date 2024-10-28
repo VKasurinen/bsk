0def912dbd4c2aab6a94187c1e3ad66e525aa9e5
VKasurinen <vaino.kasurinen@hotmail.fi>
2024-10-28 16:50:27 +0200

    User story 1

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -1,5 +1,4 @@
 import unittest
-
 from bowling import BowlingGame
 from bowling_error import BowlingError
 from frame import Frame
@@ -7,5 +6,9 @@ from frame import Frame
 
 class TestBowlingGame(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_game_created(self):
+        f = Frame(1, 5)
+        game = BowlingGame()
+        game.add_frame(f)
+        self.assertEqual(f, game.get_frame_at(0))
+
